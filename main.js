b83107f2386eced9311e3aec2c6a2aa77254a187
Elijah Schrock <70502486+ElijahSchrock@users.noreply.github.com>
2021-11-18 21:26:25 -0800

    added 5 threeejs renderers in place of div placeholders

--- main.js
+++ main.js
@@ -4,9 +4,27 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGL1Renderer ();
+const renderer2 = new THREE.WebGL1Renderer();
+const renderer3 = new THREE.WebGL1Renderer();
+const renderer4 = new THREE.WebGL1Renderer();
+const renderer5 = new THREE.WebGL1Renderer();
 
 console.log(scene)
 console.log(camera)
 console.log(renderer)
 
-document.appendChild(renderer)
+renderer.setSize(202, 100, 100);
+document.getElementById('app').appendChild(renderer.domElement)
+
+renderer2.setSize(202, 100, 100);
+document.getElementById('app2').appendChild(renderer2.domElement)
+
+
+renderer3.setSize(202, 100, 100);
+document.getElementById('app3').appendChild(renderer3.domElement)
+
+renderer4.setSize(202, 100, 100);
+document.getElementById('app4').appendChild(renderer4.domElement)
+
+renderer5.setSize(202, 100, 100);
+document.getElementById('app5').appendChild(renderer5.domElement)
